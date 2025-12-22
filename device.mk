757a821ac1ef01bc4dc40428011c0f13f06f7b50
liwhy1 <levi.toth2005@gmail.com>
2025-12-22 23:48:24 +0100

    op6893: sensors: Migrate to aidl sensors multihal

--- device.mk
+++ device.mk
@@ -448,12 +448,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service-multihal.op6893 \
-    als_correction_service.op6893 \
-    android.hardware.sensors@1.0.vendor \
-    android.hardware.sensors@2.0.vendor \
-    android.hardware.sensors@2.1.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.hardware.sensors-service.multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge \
     libpower.vendor
