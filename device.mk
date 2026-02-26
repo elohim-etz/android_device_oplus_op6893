8bbdf36814a0de2d3697a4434278db83a5b0d7f0
liwhy1 <levi.toth2005@gmail.com>
2026-02-26 17:12:09 +0100

    op6893: Build gnss service from source
     * no longer built by hw/mtk

--- device.mk
+++ device.mk
@@ -201,20 +201,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     libgatekeeper.vendor
 
-# GNSS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss.measurement_corrections@1.0.vendor \
-    android.hardware.gnss.measurement_corrections@1.1.vendor \
-    android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@1.0.vendor:64 \
-    android.hardware.gnss@1.1.vendor:64 \
-    android.hardware.gnss@2.0.vendor:64 \
-    android.hardware.gnss@2.1.vendor:64 \
-    libcurl.vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.gnss-service.mediatek
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.safe_union@1.0.vendor \
