cf225c2974ef9cfd2e8aa670bfe7160fc8a5b658
liwhy1 <levi.toth2005@gmail.com>
2026-03-02 16:43:13 +0100

    op6893: extract-files: Patch media.c2 libs with libavservices_minijail.so

--- extract-files.py
+++ extract-files.py
@@ -70,6 +70,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyalsa.so', 'libtinyalsa-v32.so'),
     ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so')
         .add_needed('libcodec2_hidl@1.0.so')
         .add_needed('libshim.so')
         .add_needed('libstagefright_foundation_v33.so'),
