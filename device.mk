72f42d64cfbd713cd6511628a1d1072be04c7cb2
liwhy1 <levi.toth2005@gmail.com>
2026-03-28 15:31:03 +0100

    op6893: Move usb service to vendor

--- device.mk
+++ device.mk
@@ -458,10 +458,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-mediatekv2
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.op6893
